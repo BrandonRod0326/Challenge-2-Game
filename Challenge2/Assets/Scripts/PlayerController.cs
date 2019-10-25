079edfe69ee3580859d7aaca1c688d38ca870709
BrandonRod0326 <54637490+BrandonRod0326@users.noreply.github.com>
2019-10-25 19:42:47 -0400

    This is Challenge 2

--- Challenge2/Assets/Scripts/PlayerController.cs
+++ Challenge2/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody2D rd2d;
     public float speed;
+    public float jump;
     public Text score;
     public Text lives;
     public Text win;
@@ -15,14 +16,21 @@ public class PlayerController : MonoBehaviour
     public AudioClip musicClipOne;
     public AudioClip musicClipTwo;
     public AudioClip musicClipThree;
+    
 
     private int scoreValue = 0;
     private int livesValue = 3;
     private bool facingRight = true;
 
+
+    private Animator anim;
+    private SpriteRenderer sprite;
+
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
         score.text = "Coins:" + scoreValue.ToString();
         lives.text = "Lives:" + livesValue.ToString();
         win.text = "";
@@ -37,6 +45,7 @@ public class PlayerController : MonoBehaviour
         float verMovement = Input.GetAxis("Vertical");
 
         rd2d.AddForce(new Vector2(hozMovement * speed, verMovement * speed));
+        rd2d.AddForce(new Vector2(hozMovement * jump, verMovement * jump));
 
         if (facingRight == false && hozMovement > 0)
         {
@@ -53,7 +62,20 @@ public class PlayerController : MonoBehaviour
             Scaler.x *= -1;
             transform.localScale = Scaler;
         }
-        
+        if (Input.GetKey("escape"))
+            Application.Quit();
+
+        if (hozMovement == 0)
+        {
+            anim.SetBool("Walk", false);
+        }
+        else
+        {
+            anim.SetBool("Walk", true);
+        }
+
+       
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -63,6 +85,12 @@ public class PlayerController : MonoBehaviour
             scoreValue += 1;
             score.text = "Coins:" + scoreValue.ToString();
             Destroy(collision.collider.gameObject);
+            if (scoreValue == 4)
+            transform.position = new Vector2(81.25f, .502f);
+            if (scoreValue == 4)
+            livesValue = 3;
+            lives.text = "Lives:" + livesValue.ToString();
+
         }
 
         if (collision.collider.tag == "Enemy")
@@ -74,23 +102,26 @@ public class PlayerController : MonoBehaviour
 
         if (scoreValue == 8)
         {
-            win.text = "You Win!";
-            musicSource.clip = musicClipThree;
+            win.text = "You Win! Game created by Brandon Rodriguez";
+            musicSource.clip = musicClipTwo;
             musicSource.Play();
+            Destroy(rd2d);
+            Destroy(anim);
+            Destroy(sprite);
         }
+        
 
         if (livesValue == 0)
         {
-            win.text = "Game Over";
-            musicSource.clip = musicClipTwo;
+            win.text = "Game Over! Game created by Brandon Rodriguez";
+            musicSource.clip = musicClipThree;
             musicSource.Play();
-        }
-
-        if (scoreValue == 4)
-        {
-            transform.position = new Vector2(77.43f, 0.503f);
+            Destroy(rd2d);
+            Destroy(anim);
+            Destroy(sprite);
         }
     }
+       
 
 
     private void OnCollisionStay2D(Collision2D collision)
@@ -100,13 +131,28 @@ public class PlayerController : MonoBehaviour
             if(Input.GetKey(KeyCode.W))
             {
                 rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+
+                anim.SetBool("Jump", true);
             }
+            else
+            {
+                anim.SetBool("Jump", false);
+            }
+
         }
+
         if (collision.collider.tag == "Tree")
-        {
+        { 
             if (Input.GetKey(KeyCode.W))
             {
-                rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+               rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+
+
+                anim.SetBool("Jump", true);
+            }
+            else
+            {
+                anim.SetBool("Jump", false);
             }
         }
     }
